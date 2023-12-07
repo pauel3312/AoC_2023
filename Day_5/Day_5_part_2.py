ab294f69d295b4bfc33a9b1a27828484773d33a6
pauel3312 <pau.lacazedieu@gmail.com>
2023-12-07 19:08:33 +0100

    beautified the code

--- Day_5/Day_5_part_2.py
+++ Day_5/Day_5_part_2.py
@@ -3,9 +3,11 @@ from Day_5_part_1 import *
 
 def format_table(i):
     table = get_table(i)
+
     for i, line in enumerate(table):
         line = [int(x) for x in line.split(' ')]
         table[i] = (line[0] - line[1], range(line[1], line[1] + line[2]))
+
     return table
 
 
@@ -50,17 +52,20 @@ def main():
     seeds = [(int(seeds_data[1:].split(" ")[i]), int(seeds_data[1:].split(" ")[i+1]))
              for i in range(0, len(seeds_data[1:].split(" ")), 2)]
     seeds = [range(i[0], i[0]+i[1]) for i in seeds]
+
     for table_index in range(1, len(path)):
         changed_seeds = []
+
         for line in format_table(table_index):
             seeds, changed_temp = apply_line_to_seeds(line, seeds)
+
             for s in changed_temp:
                 changed_seeds.append(s)
+
         for s in changed_seeds:
             seeds.append(s)
-    mins = []
-    for seed in seeds:
-        mins.append(seed.start)
+
+    mins = [(lambda x: x.start)(x) for x in seeds]
     print(min(mins))
 
 
