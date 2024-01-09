ad3d345a3cddedea5674c96559ede21b246a457d
pauel3312 <pau.lacazedieu@gmail.com>
2024-01-09 12:52:12 +0100

    Day 15 part 1 done

--- Day_15/Day_15_part_1.py
+++ Day_15/Day_15_part_1.py
@@ -1,8 +1,24 @@
+from Advent_Utils.utils import load_data
+
+
+def hash_str(str_in: str) -> int:
+    current_value = 0
+    index = 0
+    while index != len(str_in):
+        current_value += ord(str_in[index])
+        current_value *= 17
+        current_value %= 256
+        index += 1
+    return current_value
 
 
 def main():
-    None
-    
+    data = load_data(True)[0].split(',')
+    sum_hashes = 0
+    for elem in data:
+        sum_hashes += hash_str(elem)
+    return sum_hashes
+
 
 if __name__ == '__main__':
-    main()
+    print(main())
