c10a9fe9a5ccf327f53826d1a19476543659dc58
pauel3312 <pau.lacazedieu@gmail.com>
2024-01-13 00:37:46 +0100

    Day 16 part 1

--- Day_16/Day_16_part_1.py
+++ Day_16/Day_16_part_1.py
@@ -1,4 +1,5 @@
-# import os
+import time
+
 from Advent_Utils.utils import load_data
 
 data = []
@@ -22,14 +23,15 @@ def check_out_of_bounds(position: tuple[int, int]) -> bool:
 
 def beam_step(position, direction) -> None:
     new_position = (position[0] + direction[0], position[1] + direction[1])
-    if new_position == (8, 8):
-        pass
-
     if check_out_of_bounds(new_position):
         return
 
     match data[new_position[0]][new_position[1]]:
         case ".":
+            newer_position = (new_position[0] + direction[0], new_position[1] + direction[1])
+            while not check_out_of_bounds(newer_position) and data[newer_position[0]][newer_position[1]] == ".":
+                new_position = newer_position
+                newer_position = (new_position[0] + direction[0], new_position[1] + direction[1])
             beam_step(new_position, direction)
         case "/":
             new_direction = (-direction[1], -direction[0])
@@ -65,14 +67,6 @@ def beam_step(position, direction) -> None:
                 beam_step(new_position, new_direction)
 
 
-def main():
-    global data
-    data = load_data(16, 2023, False)
-    beam_starts.add(((0, 0), (0, 1)))
-    beam_step((0, 0), (0, 1))
-    return display_beams_on_data()
-
-
 def display_beams_on_data():
     new_data = ["."*len(data[0])]*len(data)
     for start, direction in beam_starts:
@@ -85,11 +79,16 @@ def display_beams_on_data():
             new_data[next_position[0]] = replace_at_index_in_string(new_data[next_position[0]],
                                                                     start[1] + (direction[1]*step),
                                                                     "#")
+            if (((data[next_position[0]][next_position[1]] in "\\/") or
+               (data[next_position[0]][next_position[1]] == "-" and direction[0] != 0) or
+               (data[next_position[0]][next_position[1]] == "|" and direction[1] != 0)) and
+               (start == (0, 0) and direction == (0, 1))):
+                wall = True
 
+            step += 1
             next_position = (start[0]+(direction[0]*step),
                              start[1]+(direction[1]*step))
 
-            step += 1
             if check_out_of_bounds(next_position) or \
                (data[next_position[0]][next_position[1]] in "\\/") or \
                (data[next_position[0]][next_position[1]] == "-" and direction[0] != 0) or \
@@ -99,6 +98,16 @@ def display_beams_on_data():
     return new_data
 
 
+def main():
+    global data
+    data = load_data(16, 2023, True)
+    beam_starts.add(((0, 0), (0, 1)))
+    beam_step((0, -1), (0, 1))
+    number_energized_tiles = 0
+    for energized_display_line in display_beams_on_data():
+        number_energized_tiles += energized_display_line.count("#")
+    return number_energized_tiles
+
+
 if __name__ == '__main__':
-    for line in main():
-        print(line)
+    print(main())
