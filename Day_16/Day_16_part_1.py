a6fd979b7cd355ce258452a90212c480d98d75c8
pauel3312 <pau.lacazedieu@gmail.com>
2024-01-12 19:37:52 +0100

    File transfer commit

--- Day_16/Day_16_part_1.py
+++ Day_16/Day_16_part_1.py
@@ -1,3 +1,4 @@
+# import os
 from Advent_Utils.utils import load_data
 
 data = []
@@ -21,6 +22,8 @@ def check_out_of_bounds(position: tuple[int, int]) -> bool:
 
 def beam_step(position, direction) -> None:
     new_position = (position[0] + direction[0], position[1] + direction[1])
+    if new_position == (8, 8):
+        pass
 
     if check_out_of_bounds(new_position):
         return
@@ -30,10 +33,14 @@ def beam_step(position, direction) -> None:
             beam_step(new_position, direction)
         case "/":
             new_direction = (-direction[1], -direction[0])
+            if (new_position, new_direction) in beam_starts:
+                return
             beam_starts.add((new_position, new_direction))
             beam_step(new_position, new_direction)
         case "\\":
             new_direction = (direction[1], direction[0])
+            if (new_position, new_direction) in beam_starts:
+                return
             beam_starts.add((new_position, new_direction))
             beam_step(new_position, new_direction)
         case "-":
@@ -41,23 +48,21 @@ def beam_step(position, direction) -> None:
                 beam_step(new_position, direction)
                 return
             for direction_offset in (-1, 1):
-                new_start_position = (new_position[0], new_position[1] + direction_offset)
-                if check_out_of_bounds(new_start_position):
-                    continue
                 new_direction = (0, direction_offset)
+                if (new_position, new_direction) in beam_starts:
+                    return
                 beam_starts.add((new_position, new_direction))
-                beam_step(new_start_position, new_direction)
+                beam_step(new_position, new_direction)
         case "|":
             if direction[1] == 0:
                 beam_step(new_position, direction)
                 return
             for direction_offset in (-1, 1):
-                new_start_position = (new_position[0] + direction_offset, new_position[1])
-                if check_out_of_bounds(new_start_position):
-                    continue
                 new_direction = (direction_offset, 0)
+                if (new_position, new_direction) in beam_starts:
+                    return
                 beam_starts.add((new_position, new_direction))
-                beam_step(new_start_position, new_direction)
+                beam_step(new_position, new_direction)
 
 
 def main():
@@ -65,9 +70,35 @@ def main():
     data = load_data(16, 2023, False)
     beam_starts.add(((0, 0), (0, 1)))
     beam_step((0, 0), (0, 1))
-    new_data = data.copy()
-    return beam_starts
+    return display_beams_on_data()
+
+
+def display_beams_on_data():
+    new_data = ["."*len(data[0])]*len(data)
+    for start, direction in beam_starts:
+        wall = False
+        step = 0
+        next_position = start
+        while not wall:
+            if check_out_of_bounds((next_position[0], start[1] + (direction[1]*step))):
+                break
+            new_data[next_position[0]] = replace_at_index_in_string(new_data[next_position[0]],
+                                                                    start[1] + (direction[1]*step),
+                                                                    "#")
+
+            next_position = (start[0]+(direction[0]*step),
+                             start[1]+(direction[1]*step))
+
+            step += 1
+            if check_out_of_bounds(next_position) or \
+               (data[next_position[0]][next_position[1]] in "\\/") or \
+               (data[next_position[0]][next_position[1]] == "-" and direction[0] != 0) or \
+               (data[next_position[0]][next_position[1]] == "|" and direction[1] != 0):
+                wall = True
+
+    return new_data
 
 
 if __name__ == '__main__':
-    print(main())
+    for line in main():
+        print(line)
